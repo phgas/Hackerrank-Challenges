de5a4ac872583d5ade1570d9c98cd6d23ff6eb6e
phgas <89270047+phgas@users.noreply.github.com>
2023-10-15 23:15:15 +0200

    Initial commit

---
+++ Problem Solving/breakingRecords.py
@@ -0,0 +1,27 @@
+def breakingRecords(scores: list) -> list:
+    """
+    This function takes a list of scores as input and returns a list that indicates the number of times a new record
+    for the highest score and the lowest score was broken.
+
+    :param scores: A list of scores.
+    :return: A list with two integers: [Number of times the highest record was broken, Number of times the lowest record was broken].
+    """
+
+    # Time complexity: O(n), where n is the number of elements in scores
+    min_score = scores[0]
+    max_score = scores[0]
+    records = [0, 0]
+
+    for score in scores:
+        if score < min_score:
+            records[1] += 1
+            min_score = score
+        elif score > max_score:
+            records[0] += 1
+            max_score = score
+
+    return records
+
+
+scores = [12, 24, 10, 24]
+print(breakingRecords(scores))
