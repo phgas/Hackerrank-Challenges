3ed256c080cc90a6527f1263dd78e176a12d43d8
phgas <89270047+phgas@users.noreply.github.com>
2023-10-17 15:26:43 +0200

    added day-of-the-programmer

---
+++ Problem Solving/day-of-the-programmer.py
@@ -0,0 +1,32 @@
+def dayOfProgrammer(year: int) -> str:
+    """
+    This function takes an integer year as input.
+    It returns a string representing the date of the 256th day of the year given.
+
+    :param year: Given year. (integer)
+    :return: Date in the format dd.mm.yyyy. (string)
+    """
+
+    if year == 1918:
+        formatted_date = "26.09.1918"
+
+    # Julian  calendar
+    elif year < 1918:
+        if year % 4 == 0:
+            return f"12.09.{year}"  # Leap year
+        else:
+            return f"13.09.{year}"  # Non-leap year
+
+    # Gregorian calendar
+    else:
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            return f"12.09.{year}"  # Leap year
+        else:
+            return f"13.09.{year}"  # Non-leap year
+
+    return formatted_date
+
+
+if __name__ == "__main__":
+    year = 2700
+    print(dayOfProgrammer(year))
